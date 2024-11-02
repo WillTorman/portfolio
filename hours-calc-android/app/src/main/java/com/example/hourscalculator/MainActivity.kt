482f62405d610ef0ec3183768ad9b53646dbf2b7
Will Torman <willtorman@gmail.com>
2024-11-01 23:38:11 -0600

    finished all functionality!

--- hours-calc-android/app/src/main/java/com/example/hourscalculator/MainActivity.kt
+++ hours-calc-android/app/src/main/java/com/example/hourscalculator/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.example.hourscalculator
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.hourscalculator.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+//    Initialize variables
+    lateinit var btnCalc : Button
+    lateinit var et_start : EditText
+    lateinit var et_end : EditText
+    lateinit var resultTv : TextView
+//
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        btnCalc = findViewById(R.id.btn_calc)
+        et_start = findViewById(R.id.et_start)
+        et_end = findViewById(R.id.et_end)
+        resultTv = findViewById(R.id.result_tv)
+
+//        Add click listener
+        btnCalc.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+//        Initialize variables
+        var startTime = et_start.text.toString().toDouble()
+        var endTime = et_end.text.toString().toDouble()
+        var result = 0.0
+//        Calculate hours when button pressed
+        when(v?.id){
+            R.id.btn_calc ->{
+                result = endTime - startTime
+            }
         }
+        resultTv.text = "Total hours: $result"
     }
 }
